086823a4fd526d68c2b3ad10d36022104fc23d62
最萌小汐 <sumneko@hotmail.com>
2021-03-10 17:41:08 +0800

    update

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,5 +1,12 @@
 import * as vscode from 'vscode';
 
+vscode.commands.registerCommand("extension.helloWorld", () => {
+	console.log("test1")
+})
+vscode.commands.registerCommand("helloWorld", () => {
+	console.log("test2")
+})
+
 vscode.languages.registerHoverProvider('lua', {
 	provideHover() {
 		return new vscode.Hover(markupContent)
